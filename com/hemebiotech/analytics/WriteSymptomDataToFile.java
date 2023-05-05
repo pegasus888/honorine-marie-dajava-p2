fbc59f02d387742b684bc452cad28e114794887a
Marie Honorine <mllthm6nr166-student@anonymous-codio.com>
2023-05-05 07:08:50 +0000

    writer implementation

--- com/hemebiotech/analytics/WriteSymptomDataToFile.java
+++ com/hemebiotech/analytics/WriteSymptomDataToFile.java
@@ -1,8 +1,43 @@
 package com.hemebiotech.analytics;
 
+import java.io.BufferedWriter;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.util.Iterator;
 import java.util.Map;
 
-// WRITE YOUR CODE HERE
+public class WriteSymptomDataToFile implements ISymptomWriter {
+
+    private String fileName;
+
+    /**
+     * Write a map in a file named result.out.
+     * One item by line with format "key:value".
+     * @param symptoms Map with ordered symptoms and occurrences
+     */
+
+
+    @Override
+    public void writeSymptoms(Map<String, Integer> symptoms) {
+
+        fileName = "result.out";
+
+
+        try {
+            FileWriter writer = new FileWriter(fileName);
+            BufferedWriter bw = new BufferedWriter(writer);
+
+            for (Map.Entry<String, Integer> mapEntry : symptoms.entrySet()) {
+                bw.write(mapEntry.getKey() + " : " + mapEntry.getValue());
+                bw.newLine();
+
+            }
+            bw.close();
+            writer.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+    }
+
+}
