812f61bd9876d637fe59d9cecb84bb94aa98bdca
Marie Honorine <mllthm6nr166-student@anonymous-codio.com>
2023-05-05 08:45:20 +0000

    main implementation

--- com/hemebiotech/analytics/Main.java
+++ com/hemebiotech/analytics/Main.java
@@ -2,5 +2,27 @@ package com.hemebiotech.analytics;
 
 import java.util.List;
 import java.util.Map;
+import java.util.SortedMap;
+import java.util.TreeMap;
 
-// WRITE YOUR CODE HERE
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        ISymptomReader myReader = new ReadSymptomDataFromFile("symptoms.txt");
+        ISymptomWriter myWriter = new WriteSymptomDataToFile();
+        AnalyticsCounter analyticsCounter = new AnalyticsCounter(myReader, myWriter);
+
+        List<String> listOfSymptoms;
+
+        listOfSymptoms = analyticsCounter.getSymptoms();
+
+        Map<String, Integer> counting = analyticsCounter.countSymptoms(listOfSymptoms);
+
+        SortedMap<String, Integer> ordering = analyticsCounter.sortSymptoms(counting);
+
+        analyticsCounter.writeSymptoms(ordering);
+
+    }
+}
