7b93393e47ca6267cbdb028db3805c35436c7f25
Marie Honorine <mllthm6nr166-student@anonymous-codio.com>
2023-05-11 20:21:07 +0000

    main implementation

--- com/hemebiotech/analytics/Main.java
+++ com/hemebiotech/analytics/Main.java
@@ -3,22 +3,27 @@ package com.hemebiotech.analytics;
 import java.util.List;
 import java.util.Map;
 import java.util.SortedMap;
-import java.util.TreeMap;
-
 
 public class Main {
-
   /**
-  * Main method implementation to run Hemebiotech Analytics Program.
+  * Runs Hemebiotech's symptoms analytics program.
+  *
   */
   public static void main(String[] args) {
+
     ISymptomReader myReader = new ReadSymptomDataFromFile("symptoms.txt");
     ISymptomWriter myWriter = new WriteSymptomDataToFile();
     AnalyticsCounter analyticsCounter = new AnalyticsCounter(myReader, myWriter);
+
     List<String> listOfSymptoms;
+
     listOfSymptoms = analyticsCounter.getSymptoms();
+
     Map<String, Integer> counting = analyticsCounter.countSymptoms(listOfSymptoms);
+
     SortedMap<String, Integer> ordering = analyticsCounter.sortSymptoms(counting);
+
     analyticsCounter.writeSymptoms(ordering);
+  
   }
 }
