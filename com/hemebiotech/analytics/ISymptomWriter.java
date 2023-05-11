4479334ecaefcb09b16d22100ce457ba38859ca6
Marie Honorine <mllthm6nr166-student@anonymous-codio.com>
2023-05-11 20:33:04 +0000

    writer implementation

--- com/hemebiotech/analytics/ISymptomWriter.java
+++ com/hemebiotech/analytics/ISymptomWriter.java
@@ -2,13 +2,12 @@ package com.hemebiotech.analytics;
 
 import java.util.Map;
 
-
-/**
- * Anything the will write symptom data on a valid data output format
- *
- */
 public interface ISymptomWriter {
 
+  /**
+   * Writes symptoms To File.
+   *
+  */
   void writeSymptoms(Map<String, Integer> symptoms);
-
+  
 }
